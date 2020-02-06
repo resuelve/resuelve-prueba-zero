68d3b54e477c8334f1191fbf737a562c75c8b8e0
Carlos Flores <carlos.flores@resuelve.mx>
2020-02-06 12:54:56 -0600

    Remove firebase fns to create a stand alone app

--- app/index.js
+++ app/index.js
@@ -3,12 +3,20 @@ const users = require('./users')
 const money = require('./money')
 const movements = require('./movements')
 const conta = require('./conta')
+const path = require('path')
 const app = express()
 
 app.use('/users', users)
 app.use('/money', money)
 app.use('/movements', movements.routes)
 app.use('/conta', conta)
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
+app.get('/:location', (req, res) => {
+  res.sendFile(path.join(__dirname, `./public/${req.params.location}.html`))
+})
 
 app.listen(1337, () => {
   console.log('localhost server online')
